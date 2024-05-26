630bec3979962bff0761ee954dc6ad621d852f68
augustommarques <54535985+augustommarques@users.noreply.github.com>
2024-05-26 05:11:51 +0000

    SOs

--- SOs/ubuntu.sh
+++ SOs/ubuntu.sh
@@ -1,6 +1,33 @@
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install zsh -y
+#Wsudo apt install zsh -y
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/augustommarques/Setup/main/SOs/ubuntu.sh)"
 
+#git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Add Docker's official GPG key:
+sudo apt-get install   apt-transport-https  software-properties-common
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl status docker
+
+sudo service docker start
+
+sudo service docker anable
+
+sudo usermod -aG docker $(whoami)
